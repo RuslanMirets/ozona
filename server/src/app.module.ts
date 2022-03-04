92fb6efd3d731dc37faff2ed5c8370fd8b210670
Ruslan <ruslan.mirets@gmail.com>
2022-03-05 00:01:25 +0300

    Mongoose and Database Setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
+import { DatabaseModule } from './core/database/database.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/core/database/database.provider.ts
@@ -0,0 +1,11 @@
+import * as mongoose from 'mongoose';
+
+export const databaseProvider = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect(
+        'mongodb+srv://admin:admin123@cluster0.68aoi.mongodb.net/ozona?retryWrites=true&w=majority',
+      ),
+  },
+];
