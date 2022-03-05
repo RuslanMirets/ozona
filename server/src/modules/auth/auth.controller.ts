c04a636d57c5ca5ee90b638895c1af7dc1b29bdb
Ruslan <ruslan.mirets@gmail.com>
2022-03-05 18:31:36 +0300

    Guards

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { DoesUserExist } from 'src/core/guards/doesUserExist.guard';
+import { LocalAuthGuard } from 'src/core/guards/local-auth.guard';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { AuthService } from './auth.service';
 
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return await this.authService.login(req.user);
--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -2,10 +2,12 @@ import { DatabaseModule } from './../../core/database/database.module';
 import { Module } from '@nestjs/common';
 import { userProvider } from './user.provider';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [DatabaseModule],
   providers: [UserService, ...userProvider],
-  exports: [UserService]
+  exports: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
