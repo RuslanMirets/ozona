92d0089b471b8f426d06892eab82c360331dd6a2
Ruslan <ruslan.mirets@gmail.com>
2022-03-06 16:35:03 +0300

    Role module [server]

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { RoleModule } from './modules/role/role.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UserModule, AuthModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
