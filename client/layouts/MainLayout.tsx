ac867b1260f5f27e5e393b5240f135db654b79cc
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-03-09 14:44:23 +0300

    Added _document.tsx

--- client/layouts/MainLayout.tsx
+++ client/layouts/MainLayout.tsx
@@ -19,10 +19,6 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title, description, k
         <meta name="keywords" content={keywords || 'Магазин товаров'} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,800,900&display=swap"
-        />
       </Head>
       <div className="wrapper">
         <Header />
