901baa52a1aa348e2c596b7db16dbf2f85cb0fe8
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-03-09 13:17:29 +0300

    Validation [server]

--- server/src/modules/user/dto/create-user.dto.ts
+++ server/src/modules/user/dto/create-user.dto.ts
@@ -1,7 +1,13 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Введите имя' })
   readonly name: string;
 
+  @IsNotEmpty({ message: 'Введите email' })
+  @IsEmail(undefined, { message: 'Некорректная почта' })
   readonly email: string;
-  
+
+  @MinLength(6, { message: 'Пароль должен быть минимум 6 символов' })
   readonly password: string;
 }
